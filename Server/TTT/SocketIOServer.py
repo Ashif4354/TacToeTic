28042bada541767f90b21d042975987a934597e2
Ashif4354 <ashif.abhas@gmail.com>
2024-11-18 08:43:18 +0530

    Add dotenv support for environment variables and configure CORS origins in SocketIOServer

--- Server/TTT/SocketIOServer.py
+++ Server/TTT/SocketIOServer.py
@@ -1,8 +1,38 @@
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    pass
+
 from socketio import AsyncServer
+from os import environ
 
 from .namespaces.Game import Game
 
-sio = AsyncServer(async_mode='asgi', cors_allowed_origins='*')
+sio = AsyncServer(
+    async_mode='asgi', 
+    cors_allowed_origins=[
+        'https://ttt-dg.netlify.app',
+        'https://admin.socket.io'
+    ],
+    # cors_allowed_origins='*', # for development
+)
+
+sio.instrument(
+    auth = {
+        'username': environ['username'],
+        'password': environ['password']
+    },
+    server_id="TacToeTic",
+    server_stats_interval=30
+)
+
+@sio.on('test')
+async def ashif(sid, data):
+    return {
+        'data': data,
+        'sid': sid
+    }
 
 sio.register_namespace(Game('/game'))
 
